5fd6e6cfad35156a15dda0faa9f8e949dfcc08fb
SpicyOwl <duanxu@bc.edu>
2021-03-10 01:54:58 -0600

    working calculator, working on socket shareing feature now

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,27 +1,103 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React,{Component} from 'react';
+import './App_cal.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
+import HistoryComponent from './components/HistoryComponent';
 
-import "./index.css";
-import Home from "./Home/Home";
-import ChatRoom from "./ChatRoom/ChatRoom";
-import App_cal from "./App_cal";
+window.histArr = [];
+class App extends Component {
+    
+    constructor(){
+        super();
 
-function App() {
-  return (
-    <div>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/:roomId" component={ChatRoom} />
-      </Switch>
-    </Router>
+        this.state = {
+            result: "",
+            history: "",
+            //insert past history to the historyArr?
+            historyArr: Array(10).fill(null),
+            
+        }
+    }
+    //define onClick function that used in keypadComponent.js
+
+    onClick=button=>{
+        if(button === "="){
+            // console.log("calculate");
+            this.setState({
+                history: this.state.result+'='+ eval(this.state.result),
+            })
+            window.histArr.unshift(this.state.history);
+            console.log(window.histArr);
+            if(window.histArr.length >=9){
+                console.log("excess length");
+                window.histArr.pop();
+
+            }
+
+
+            this.calculate()
+        }else if(button === "C"){
+            console.log("reset");
+            this.reset();
+        }else if(button === "CE"){
+            // console.log("backspace");
+            this.backspace();
+        }else{
+            // console.log("before else: " + this.state.result);
+            this.setState({
+                result: this.state.result + button,
+            })
+        }
+    };
+
+    calculate = ()=>{
+        try{
+            this.setState({
+                result:(eval(this.state.result) || "") + "",
+            })
+        }catch(e){
+            this.setState({
+                result:"error"
+            })
+        }
+
+    };
 
     
-        
-        
+    reset=()=>{
+        this.setState({
+            result:""
+        })
+    };
+    backspace=()=>{
+        this.setState({
+            result: this.state.result.slice(0,-1)
+        })
 
-    </div>
-  );
+    }
+
+
+    render(){
+        return(
+            <div>
+                <div className = "calculator-body">
+                    <h5>
+                        A Calculator
+                    </h5>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                    <HistoryComponent history={this.state.history}/>
+                    <HistoryComponent history={window.histArr+ "\n"}/>
+                </div>
+                <div>
+                    <h5>
+                        Shared history Area
+                    </h5>
+                    
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
